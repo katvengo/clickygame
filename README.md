44453c536a411ff181710eb0673ecdfcec5b6c0b
Kat Vengo <katvengo@gmail.com>
2019-07-12 18:19:45 -0700

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-This is a clicky game called the Dunder Mifflin Memory game. Its to test your short-term memory. Click on an image and earn a point, but don't click it again or else you'll go back to zero! 
-
---
+++ precache-manifest.73de7c262de6532f600aab6f1f4aa701.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193347e357241d8ad9dad0ee7719057d",
+    "url": "/dundermifflinmemorygame/index.html"
+  },
+  {
+    "revision": "02cf095d834868103ecb",
+    "url": "/dundermifflinmemorygame/static/css/main.ccc81bf0.chunk.css"
+  },
+  {
+    "revision": "a330c626bf808a2b61f6",
+    "url": "/dundermifflinmemorygame/static/js/2.31b82c5e.chunk.js"
+  },
+  {
+    "revision": "02cf095d834868103ecb",
+    "url": "/dundermifflinmemorygame/static/js/main.8f77bae9.chunk.js"
+  },
+  {
+    "revision": "5b0b1fbe717749b3c33c",
+    "url": "/dundermifflinmemorygame/static/js/runtime~main.bfd0cc0b.js"
+  },
+  {
+    "revision": "4fdde86874d827fcd1e7c47fc46e89bb",
+    "url": "/dundermifflinmemorygame/static/media/notebook.4fdde868.png"
+  }
+]);
