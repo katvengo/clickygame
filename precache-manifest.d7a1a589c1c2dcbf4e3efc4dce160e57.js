c7e2bb11d9b8a07dabc1df2c27f0c5c85562aae5
Kat Vengo <katvengo@gmail.com>
2019-07-13 21:46:49 -0700

    Updates

---
+++ precache-manifest.d7a1a589c1c2dcbf4e3efc4dce160e57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff3e542d311babe1411c0a30e131b321",
+    "url": "/dundermifflinmemorygame/index.html"
+  },
+  {
+    "revision": "29eb061d54047a6b0799",
+    "url": "/dundermifflinmemorygame/static/css/main.ccc81bf0.chunk.css"
+  },
+  {
+    "revision": "8e4d9d753adb3ee69c4c",
+    "url": "/dundermifflinmemorygame/static/js/2.98410570.chunk.js"
+  },
+  {
+    "revision": "29eb061d54047a6b0799",
+    "url": "/dundermifflinmemorygame/static/js/main.a5b8efd2.chunk.js"
+  },
+  {
+    "revision": "5b0b1fbe717749b3c33c",
+    "url": "/dundermifflinmemorygame/static/js/runtime~main.bfd0cc0b.js"
+  },
+  {
+    "revision": "4fdde86874d827fcd1e7c47fc46e89bb",
+    "url": "/dundermifflinmemorygame/static/media/notebook.4fdde868.png"
+  }
+]);
